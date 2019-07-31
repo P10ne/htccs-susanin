a43dea83fc886170bc5754bc74372909688b74b8
Першин Александр <apershin@htc-cs.ru>
2019-07-31 15:05:02 +0400

    Шапка

--- config/pages.js
+++ config/pages.js
@@ -3,5 +3,10 @@ module.exports = [
         filename: 'index.html',
         inject: 'body',
         template: './src/html/pages/index.pug'
+    },
+    {
+        filename: 'components.html',
+        inject: 'body',
+        template: './src/html/pages/components.pug'
     }
 ];
