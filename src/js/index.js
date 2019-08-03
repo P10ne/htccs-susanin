32557c6440b160215fc85b513fd937b3de2ceb5a
p10ne <i-smart97@mail.ru>
2019-08-03 21:34:15 +0300

    Первая неделя, компоненты

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,25 @@ import '../css/index.pcss';
 
 /* eslint no-console: "off" */
 console.log('hello world');
+
+window.onload = function() {
+    var cityChanger = (function() {
+        let cities = ["Ижевск", "Нижний Новгород"];
+        let cityField = document.querySelector(".city");
+        let citiesList = document.querySelector(".city-change-container");
+        cityField.addEventListener('click', function() {
+            for(let i=0; i<cities.length; i++) {
+                let cityLi = document.createElement("li");
+                cityLi.className = "city-change__el";
+                cityLi.innerHTML = cities[i];
+                citiesList.appendChild(cityLi);
+            }
+        });
+        citiesList.addEventListener("click", function(e) {
+            cityField.innerText = e.target.innerText;
+        });
+    });
+    cityChanger();
+}
+
+
