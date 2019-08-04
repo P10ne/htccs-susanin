e8838dc4aa07b7abbf1b8b45485ce39a6ea05c39
p10ne <apershin@htc-cs.ru>
2019-08-04 22:59:11 +0400

    Выбор города и форма поиска в шапке

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,56 @@
 import '../css/index.pcss';
 
-/* eslint no-console: "off" */
-console.log('hello world');
-
 window.onload = function() {
-    var cityChanger = (function() {
-        let cities = ["Ижевск", "Нижний Новгород"];
-        let cityField = document.querySelector(".city");
-        let citiesList = document.querySelector(".city-change-container");
-        cityField.addEventListener('click', function() {
-            for(let i=0; i<cities.length; i++) {
-                let cityLi = document.createElement("li");
-                cityLi.className = "city-change__el";
-                cityLi.innerHTML = cities[i];
-                citiesList.appendChild(cityLi);
-            }
-        });
-        citiesList.addEventListener("click", function(e) {
-            cityField.innerText = e.target.innerText;
-        });
+    cityChanging();
+}
+
+function cityChanging() {
+    let citiesContainer = document.querySelector('.city-change-container'),
+        cities = getCities(),
+        city = document.querySelector('.city'),
+        city__value = document.querySelector('.city__value'),
+        city__svg = document.querySelector('.city__svg'),
+        citiesContainerIsOpened = false;
+
+    initCitiesContainer(citiesContainer, cities);
+
+    citiesContainer.addEventListener('click', function(e) {
+        citiesContainer.style.display = 'none';
+        citiesContainerIsOpened = false;
+        if(city__value.innerText != e.target.innerText) {
+            setCity(e.target.innerText);
+        }
+        e.stopPropagation();
     });
-    cityChanger();
+
+    city.addEventListener('click', function(e) {
+        citiesContainer.style.display = 'block';
+        citiesContainerIsOpened = true;
+    });
+
+    document.addEventListener('click', function(e) {
+        if(citiesContainerIsOpened && e.target != city && e.target != city__value && e.target != city__svg) {
+            citiesContainer.style.display = 'none';
+            citiesContainerIsOpened = false;
+        }
+    });
+
+    function getCities() {
+        return ["Ижевск", "Нижний Новгород"];
+    }
+
+    function initCitiesContainer(citiesContainer, cities) {
+        for(let i=0; i<cities.length; i++) {
+            let cityLi = document.createElement("li");
+            cityLi.className = "city-change__el";
+            cityLi.innerHTML = cities[i];
+            citiesContainer.appendChild(cityLi);
+        }
+    }
+
+    function setCity(newCity) {
+        city__value.innerText = newCity;
+    }
 }
 
 
